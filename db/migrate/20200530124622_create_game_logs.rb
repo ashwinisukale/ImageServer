c3e4a89007f9ad2befdb931fa8a68bd62d84e0e5
Ashwini Sukale <ashwinisukale16@gmail.com>
2020-05-30 18:27:52 +0530

    Deploy games log

--- db/migrate/20200530124622_create_game_logs.rb
+++ db/migrate/20200530124622_create_game_logs.rb
@@ -1,5 +1,7 @@
 class CreateGameLogs < ActiveRecord::Migration[5.2]
   def change
+    drop_table :game_logs
+
     create_table :game_logs do |t|
       t.references :image, foreign_key: true
       t.datetime :capture_time
