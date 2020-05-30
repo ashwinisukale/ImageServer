e53f464c0781d17ed341fa19b6cf025c261d6a06
Ashwini Sukale <ashwinisukale16@gmail.com>
2020-05-30 16:12:27 +0530

    Deploy to Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "images#index"
   resources :images, only: [:create, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
